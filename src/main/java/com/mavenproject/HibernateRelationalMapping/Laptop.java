cf8cbe389219cd4a56381469efa62cf8a643afca
snigdha <sunny.rangineni@gmail.com>
2024-06-05 15:50:16 -0500

    Many to One relation for laptop to Student

--- src/main/java/com/mavenproject/HibernateRelationalMapping/Laptop.java
+++ src/main/java/com/mavenproject/HibernateRelationalMapping/Laptop.java
@@ -2,12 +2,15 @@ package com.mavenproject.HibernateRelationalMapping;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Laptop {
 	@Id
 	private int lid;
 	private String laname;
+	@ManyToOne
+	private Student student;
 	
 	public int getLid() {
 		return lid;
@@ -23,7 +26,13 @@ public class Laptop {
 	}
 	@Override
 	public String toString() {
-		return "Laptop [lid=" + lid + ", laname=" + laname + "]";
+		return "Laptop [lid=" + lid + ", laname=" + laname + ", student=" + student + "]";
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
 	}
 	
 }

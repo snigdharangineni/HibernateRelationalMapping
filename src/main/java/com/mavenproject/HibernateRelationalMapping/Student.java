932e8e8da7ef95378ba827c7a3ecd4431cb621f5
snigdha <sunny.rangineni@gmail.com>
2024-06-05 15:52:01 -0500

    Mapped by annotation for Student to recongnize the Laptop foreign key

--- src/main/java/com/mavenproject/HibernateRelationalMapping/Student.java
+++ src/main/java/com/mavenproject/HibernateRelationalMapping/Student.java
@@ -15,7 +15,7 @@ public class Student {
 	private int rollno;
 	private String name;
 	private int marks;
-	@OneToMany
+	@OneToMany(mappedBy="student")
 	private List<Laptop> laptop = new ArrayList<>();
 	
 	public List<Laptop> getLaptop() {
